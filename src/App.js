283ff1947a326365cd6b3b0fdf6b1ce5fea6eb8f
Denali Tonn <87244677+DenaliTonn@users.noreply.github.com>
2022-11-06 17:37:00 -0800

    Add the delete functionality

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ export default function App() {
     { id: 1, content: "Meet Masao for Udon" },
     { id: 2, content: "Fix up these horrible React notes" },
   ]);
+  function deleteTodo(id) {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }
   return (
     <div className="App">
       <h1>Things To Do</h1>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} deleter={deleteTodo} />
     </div>
   );
 }
